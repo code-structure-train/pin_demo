adab14e8c0ea5af857b224e845afc95f1961a1ba
SuWeipeng <weipeng.su@outlook.com>
2019-07-24 21:26:43 +0800

    main() 函数优先级是 10（在rtconfigh.h中配置），如果不通过 rt_thread_delay() 阻塞延时，则优先级为 25 的串口线程永远不会调用。

--- Src-rtt/entry.c
+++ Src-rtt/entry.c
@@ -195,6 +195,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    rt_thread_delay(1);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
--- Src-rtt/serial_test.c
+++ Src-rtt/serial_test.c
@@ -35,9 +35,8 @@ static void serial_thread_entry(void *parameter)
             /* 阻塞等待接收信号量，等到信号量后再次读取数据 */
             rt_sem_take(&rx_sem, RT_WAITING_FOREVER);
         }
-        /* 读取到的数据通过串口错位输出 */
-        ch = ch + 1;
         rt_device_write(serial, 0, &ch, 1);
+        rt_thread_delay(1);
     }
 }
 
